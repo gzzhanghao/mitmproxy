1902aece105edf7348b8bafd8c7e6471cef26195
Jason <jason.daurus@gmail.com>
2016-06-19 14:11:30 -0400

    [web] remove makeList from ducks/utils/list.js

--- web/src/js/ducks/eventLog.js
+++ web/src/js/ducks/eventLog.js
@@ -1,17 +1,11 @@
-import makeList from "./utils/list"
-import {updateViewFilter, updateViewList} from "./utils/view"
+import makeList from './utils/list'
+import {updateViewFilter, updateViewList} from './utils/view'
+
+import {fetchApi, reduceList} from '../utils'
 
 const TOGGLE_FILTER = 'TOGGLE_EVENTLOG_FILTER'
 const TOGGLE_VISIBILITY = 'TOGGLE_EVENTLOG_VISIBILITY'
-export const UPDATE_LOG = "UPDATE_EVENTLOG"
-
-const {
-    reduceList,
-    updateList,
-    fetchList,
-    addItem,
-} = makeList(UPDATE_LOG, "/events")
-
+export const UPDATE_LOG = 'UPDATE_EVENTLOG'
 
 const defaultState = {
     visible: false,
@@ -24,7 +18,7 @@ const defaultState = {
     filteredEvents: [],
 }
 
-export default function reducer(state = defaultState, action) {
+export default function reduce(state = defaultState, action) {
     switch (action.type) {
         case TOGGLE_FILTER:
             const filter = {
@@ -45,7 +39,7 @@ export default function reducer(state = defaultState, action) {
                 visible: !state.visible
             }
         case UPDATE_LOG:
-            const events = reduceList(state.events, action)
+            const events = reduceList(UPDATE_LOG, state.events, action)
             return {
                 ...state,
                 events,
@@ -66,9 +60,82 @@ export default function reducer(state = defaultState, action) {
 export function toggleEventLogFilter(filter) {
     return {type: TOGGLE_FILTER, filter}
 }
+
 export function toggleEventLogVisibility() {
     return {type: TOGGLE_VISIBILITY}
 }
+
+function addItem(item) {
+    return {
+        type: UPDATE_LOG,
+        cmd: ADD,
+        item
+    }
+}
+
+function updateItem(item) {
+    return {
+        type: UPDATE_LOG,
+        cmd: UPDATE,
+        item
+    }
+}
+
+function removeItem(item) {
+    return {
+        type: UPDATE_LOG,
+        cmd: REMOVE,
+        item
+    }
+}
+
+
+function updateList(event) {
+    /* This action creater takes all WebSocket events */
+    return dispatch => {
+        switch (event.cmd) {
+            case "add":
+                return dispatch(addItem(event.data))
+            case "update":
+                return dispatch(updateItem(event.data))
+            case "remove":
+                return dispatch(removeItem(event.data))
+            case "reset":
+                return dispatch(fetchList())
+            default:
+                console.error("unknown list update", event)
+        }
+    }
+}
+
+function requestList() {
+    return {
+        type: UPDATE_LOG,
+        cmd: REQUEST_LIST,
+    }
+}
+
+function receiveList(list) {
+    return {
+        type: UPDATE_LOG,
+        cmd: RECEIVE_LIST,
+        list
+    }
+}
+
+function fetchList() {
+    return dispatch => {
+
+        dispatch(requestList())
+
+        return fetchApi("/events").then(response => {
+            return response.json().then(json => {
+                dispatch(receiveList(json.data))
+            })
+        })
+    }
+}
+
 let id = 0
 export function addLogEntry(message, level = "web") {
     return addItem({
--- web/src/js/ducks/utils/list.js
+++ web/src/js/ducks/utils/list.js
@@ -15,152 +15,77 @@ const defaultState = {
     indexOf: {},
 }
 
-export default function makeList(actionType, fetchURL) {
-    function reduceList(state = defaultState, action = {}) {
+function reduceList(actionType, state = defaultState, action = {}) {
 
-        if (action.type !== actionType) {
-            return state
+    if (action.type !== actionType) {
+        return state
+    }
+
+    // Handle cases where we finished fetching or are still fetching.
+    if (action.cmd === RECEIVE_LIST) {
+        let s = {
+            isFetching: false,
+            actionsDuringFetch: [],
+            list: action.list,
+            byId: {},
+            indexOf: {}
         }
+        for (let i = 0; i < action.list.length; i++) {
+            let item = action.list[i]
+            s.byId[item.id] = item
+            s.indexOf[item.id] = i
+        }
+        for (action of state.actionsDuringFetch) {
+            s = reduceList(actionType, s, action)
+        }
+        return s
+    } else if (state.isFetching) {
+        return {
+            ...state,
+            actionsDuringFetch: [...state.actionsDuringFetch, action]
+        }
+    }
 
-        // Handle cases where we finished fetching or are still fetching.
-        if (action.cmd === RECEIVE_LIST) {
-            let s = {
-                isFetching: false,
-                actionsDuringFetch: [],
-                list: action.list,
-                byId: {},
-                indexOf: {}
+    let list, itemIndex
+    switch (action.cmd) {
+        case ADD:
+            return {
+                list: [...state.list, action.item],
+                byId: {...state.byId, [action.item.id]: action.item},
+                indexOf: {...state.indexOf, [action.item.id]: state.list.length},
             }
-            for (let i = 0; i < action.list.length; i++) {
-                let item = action.list[i]
-                s.byId[item.id] = item
-                s.indexOf[item.id] = i
-            }
-            for (action of state.actionsDuringFetch) {
-                s = reduceList(s, action)
-            }
-            return s
-        } else if (state.isFetching) {
+
+        case UPDATE:
+
+            list = [...state.list]
+            itemIndex = state.indexOf[action.item.id]
+            list[itemIndex] = action.item
             return {
                 ...state,
-                actionsDuringFetch: [...state.actionsDuringFetch, action]
+                list,
+                byId: {...state.byId, [action.item.id]: action.item},
             }
-        }
 
-        let list, itemIndex
-        switch (action.cmd) {
-            case ADD:
-                return {
-                    list: [...state.list, action.item],
-                    byId: {...state.byId, [action.item.id]: action.item},
-                    indexOf: {...state.indexOf, [action.item.id]: state.list.length},
-                }
-
-            case UPDATE:
-
-                list = [...state.list]
-                itemIndex = state.indexOf[action.item.id]
-                list[itemIndex] = action.item
-                return {
-                    ...state,
-                    list,
-                    byId: {...state.byId, [action.item.id]: action.item},
-                }
-
-            case REMOVE:
-                list = [...state.list]
-                itemIndex = state.indexOf[action.item.id]
-                list.splice(itemIndex, 1)
-                return {
-                    ...state,
-                    list,
-                    byId: {...state.byId, [action.item.id]: undefined},
-                    indexOf: {...state.indexOf, [action.item.id]: undefined},
-                }
-
-            case REQUEST_LIST:
-                return {
-                    ...state,
-                    isFetching: true
-                }
-
-            default:
-                console.debug("unknown action", action)
-                return state
-        }
-    }
-
-    function addItem(item) {
-        return {
-            type: actionType,
-            cmd: ADD,
-            item
-        }
-    }
-
-    function updateItem(item) {
-        return {
-            type: actionType,
-            cmd: UPDATE,
-            item
-        }
-    }
-
-    function removeItem(item) {
-        return {
-            type: actionType,
-            cmd: REMOVE,
-            item
-        }
-    }
-
-
-    function updateList(event) {
-        /* This action creater takes all WebSocket events */
-        return dispatch => {
-            switch (event.cmd) {
-                case "add":
-                    return dispatch(addItem(event.data))
-                case "update":
-                    return dispatch(updateItem(event.data))
-                case "remove":
-                    return dispatch(removeItem(event.data))
-                case "reset":
-                    return dispatch(fetchList())
-                default:
-                    console.error("unknown list update", event)
+        case REMOVE:
+            list = [...state.list]
+            itemIndex = state.indexOf[action.item.id]
+            list.splice(itemIndex, 1)
+            return {
+                ...state,
+                list,
+                byId: {...state.byId, [action.item.id]: undefined},
+                indexOf: {...state.indexOf, [action.item.id]: undefined},
             }
-        }
+
+        case REQUEST_LIST:
+            return {
+                ...state,
+                isFetching: true
+            }
+
+        default:
+            console.debug("unknown action", action)
+            return state
     }
-
-    function requestList() {
-        return {
-            type: actionType,
-            cmd: REQUEST_LIST,
-        }
-    }
-
-    function receiveList(list) {
-        return {
-            type: actionType,
-            cmd: RECEIVE_LIST,
-            list
-        }
-    }
-
-    function fetchList() {
-        return dispatch => {
-
-            dispatch(requestList())
-
-            return fetchApi(fetchURL).then(response => {
-                return response.json().then(json => {
-                    dispatch(receiveList(json.data))
-                })
-            })
-        }
-    }
-
-
-    return {reduceList, updateList, fetchList, addItem, updateItem, removeItem,}
 }
+
